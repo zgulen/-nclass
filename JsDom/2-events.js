cee57c808f3aece201be443596893ede0d196ca9
zgulen <gulenzubeyir@gmail.com>
2022-05-28 13:57:56 +0300

    Best lesson so far

--- JsDom/2-events.js
+++ JsDom/2-events.js
@@ -6,3 +6,61 @@ console.log('****** EVENTS *******');
 
 //* EXAMPLE-1 (Mouse Over, Mouse Out )
 //* -------------------------------------------------------
+
+// const header = document.getElementById("header").onmouseover = function (){
+    
+//     document.getElementById('header').style.color = "red"
+
+// };
+
+
+const header = document.getElementById("header")
+header.onmouseover = function (){
+    header.style.color = "red"
+};
+header.onmouseout = function (){
+    header.style.color = "yellow"
+};
+
+
+const img = document.querySelector("img")
+img.addEventListener("mouseover",() =>{
+    img.style.transform = "scaleX(1.5)"
+} )
+
+img.addEventListener("mouseout",() =>{
+    img.style.transform = "translateX(-5px)"
+} )
+
+const button = document.querySelector("#btn");
+button.addEventListener("click", function (event){
+    document.querySelector("body").style.backgroundImage = "linear-gradient(to right, purple, yellow)";
+    button.textContent = "Search"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
